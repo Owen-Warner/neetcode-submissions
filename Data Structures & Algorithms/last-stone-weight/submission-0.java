1316510cedcaf7d156855d6ba3aa29fa18e11ac7
Owen Warner <54912409+Owen-Warner@users.noreply.github.com>
2026-07-19 14:30:02 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int s : stones){
+            pq.add(s);
+        }
+        while(pq.size() > 1){
+            int y = pq.poll();
+            int x = pq.poll();
+            if(y != x){
+                pq.add(y - x);
+            }
+        }
+        if(pq.isEmpty()){
+            return 0;
+        }
+        return pq.poll();
+    }
+}
